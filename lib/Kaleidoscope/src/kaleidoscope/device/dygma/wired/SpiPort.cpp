318f2b61ff99274f9a6d15d8eaa5699c93ec7462
Noteolvides <gustavogomezlopezdesanroman@gmail.com>
2023-02-10 16:33:01 +0100

    refactor: time to recomunicate
    Signed-off-by: Noteolvides <gustavogomezlopezdesanroman@gmail.com>

--- lib/Kaleidoscope/src/kaleidoscope/device/dygma/wired/SpiPort.cpp
+++ lib/Kaleidoscope/src/kaleidoscope/device/dygma/wired/SpiPort.cpp
@@ -22,6 +22,7 @@
 #include "SpiPort.h"
 #include "hardware/spi.h"
 #include "hardware/dma.h"
+#include "kaleidoscope/device/dygma/Wired.h"
 
 namespace kaleidoscope::device::dygma::wired {
 
@@ -118,7 +119,7 @@ bool SpiPort::sendPacket(Packet *data) {
 }
 
 uint8_t SpiPort::readFrom(uint8_t *data, size_t length) {
-  if (millis() - lasTimeCommunication > 1000) {
+  if (millis() - lasTimeCommunication > 200) {
     return 0;
   }
   if (queue_is_empty(&rxMessages)) {
@@ -152,6 +153,10 @@ void SpiPort::irq() {
     gpio_put(spiSettings.reset, true);
     return;
   }
+
+//  if (spiSettings.rxMessage.context.command == IS_DEAD) {
+//    WiredHands::initializeSide(spiSettings.rxMessage.context.device - 1);
+//  }
 
   SpiPort &spi             = spiSettings.rxMessage.context.device == KEYSCANNER_DEFY_RIGHT ? spi_1 : spi_0;
   spi.sideCommunications   = spiSettings.rxMessage.context.device;
