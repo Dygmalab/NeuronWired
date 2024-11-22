6be3b2b8a383d0d3e616a11ff4e540e59ac94160
gonza2206 <gonzalopez2206@gmail.com>
2024-11-22 10:56:40 -0300

    refactor: CRC
    Signed-off-by: gonza2206 <gonzalopez2206@gmail.com>

--- lib/CRC/src/CRC_wrapper.h
+++ lib/CRC/src/CRC_wrapper.h
@@ -1,9 +1,11 @@
+
+
 #ifndef CRC_H_
 #define CRC_H_
+
 #include "stdio.h"
 
-uint32_t crc32(const void *ptr, uint32_t len);
-
+uint32_t crc32(const uint8_t *ptr, uint32_t len);
 uint8_t crc8(uint8_t const msg[], uint32_t len);
 
 #endif
